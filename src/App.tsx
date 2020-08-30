c42465dc352230f79a785661de7c7b32da3a178f
Glauber Brack <glauber@brack.com.br>
2020-08-30 19:24:07 -0300

    installing lib to navigation

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import 'react-native-gesture-handler';
+
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { View } from 'react-native';
+
+const App: React.FC = () => {
+    return (
+        <NavigationContainer>
+            <View/>
+        </NavigationContainer>
+    );
+}
+
+export default App
