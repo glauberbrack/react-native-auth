c4a8e0f5a55514283268d4597224ed3fb71e1a49
Glauber Brack <glauber@brack.com.br>
2020-08-30 19:48:23 -0300

    usign routes on App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,13 @@ import 'react-native-gesture-handler';
 
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { View } from 'react-native';
+
+import Routes from './routes'
 
 const App: React.FC = () => {
     return (
         <NavigationContainer>
-            <View/>
+            <Routes/>
         </NavigationContainer>
     );
 }
