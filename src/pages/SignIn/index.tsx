5c312c55d4288c6eef0fc57a6a9e4f8f846bf0aa
Glauber Brack <glauber@brack.com.br>
2020-08-30 19:59:22 -0300

    editing auth service and adding handleSignIn on SignIn page

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,14 +1,24 @@
 import React from 'react';
-import { View, Button } from 'react-native';
-
-// import { Container } from './styles';
+import { View, Button, StyleSheet } from 'react-native';
+import { signIn } from '../../services/auth';
 
 const SignIn: React.FC = () => {
+  
+  function handleSignIn() {
+    signIn().then((response) => {
+      console.log(response)
+    })
+  }
+
   return (
-    <View>
-        <Button title="Sign In" onPress={() => {}} />
+    <View style={styles.container}>
+        <Button title="Sign In" onPress={handleSignIn} />
     </View>
   );
 }
 
 export default SignIn;
+
+const styles = StyleSheet.create({
+  container: { flex: 1, justifyContent: 'center'}
+});
